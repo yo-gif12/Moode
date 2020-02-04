b852db7d10e70ad73afb86383b14c2f554552215
Axel Charel <axelcharel@MacBook-Pro-de-Axel.local>
2020-02-04 14:18:29 +0100

    Add JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,4 @@
-// function click() {
-//   let li = document.querySelector(".pattern__color");
-//   li.addEventListener("click", function() {});
-// }
-
-// function remove(pattern){
-//   pattern.classList.remove("is-visible")
-// }
-
 // Axel JS
-
-// Déclaration des éléments
 
 let patternBlue = document.querySelector(".pattern__color--blue");
 let patternRed = document.querySelector(".pattern__color--red");
@@ -18,10 +7,8 @@ let patternWhite = document.querySelector(".pattern__color--white");
 let patternDark = document.querySelector(".pattern__color--dark");
 let selectedElement = document.querySelector(".normal__color.is-selected");
 
-// Tableau des couleurs
 var colors = [patternBlue, patternRed, patternGreen, patternWhite, patternDark];
 
-// Le listener à remplir
 function click() {
   let li = document.querySelector(".pattern__color");
 
@@ -31,10 +18,8 @@ function click() {
 function chooseColors() {
   let selectedElement = document.querySelector(".normal__color.is-selected");
 
-  // On cache tout par défaut, ça evite de retirer une a une la classe
   hideAll();
 
-  // Et selon le choix, on a plus qu'à rajouter la classe is-visible sur le bon
   if (selectedElement.classList.contains("normal--blue")) {
     patternBlue.classList.add("is-visible");
   } else if (selectedElement.classList.contains("normal--red")) {
@@ -45,18 +30,9 @@ function chooseColors() {
     patternWhite.classList.add("is-visible");
   } else if (selectedElement.classList.contains("normal--dark")) {
     patternDark.classList.add("is-visible");
-
-    patternBlue.classList.remove("is-visible");
-
-    patternRed.classList.remove("is-visible");
-
-    patternGreen.classList.remove("is-visible");
-
-    patternWhite.classList.remove("is-visible");
   }
 }
 
-// Permet de retirer à tous les items du tableau "colors" la classe "is-visible"
 function hideAll() {
   colors.forEach(function(item, index, array) {
     item.classList.remove("is-visible");
